a529ef16986c72f4b6bded18100592b4f6da6fbb
Daniel Serna <daniel.serna@sancorsalud.com.ar>
2022-04-20 20:03:30 -0500

    ajuste scripts base de datos

--- 2-secuencias/seq_db_nrc2021.sql
+++
@@ -1,5 +0,0 @@
-create sequence seq_db_nrc2021
-  start with 1
-  increment by 1
-  maxvalue 9999999
-  minvalue 1;
--- 2-secuencias/seq_municipio.sql
+++
@@ -1,5 +0,0 @@
- create sequence taurischema.seq_municipio
-  start with 1
-  increment by 1
-  maxvalue 9999999
-  minvalue 1;
